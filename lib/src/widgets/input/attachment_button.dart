caed0e779438094af483e288f89cf1c13f0cf2e3
Chengming Zhang <chengmingzhang@Chengmings-MacBook-Pro.local>
2023-05-23 22:57:57 -0700

    changed Icon

--- lib/src/widgets/input/attachment_button.dart
+++ lib/src/widgets/input/attachment_button.dart
@@ -49,11 +49,9 @@ class AttachmentButton extends StatelessWidget {
                   ),
                 )
               : InheritedChatTheme.of(context).theme.attachmentButtonIcon ??
-                  Image.asset(
-                    'assets/icon-attachment.png',
-                    color: InheritedChatTheme.of(context).theme.inputTextColor,
-                    package: 'flutter_chat_ui',
-                  ),
+                  Icon(Icons.mic,
+                      color:
+                          InheritedChatTheme.of(context).theme.inputTextColor),
           onPressed: isLoading ? null : onPressed,
           padding: padding,
           splashRadius: 24,
